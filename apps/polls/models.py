318faddcdf7bbcdeb49693c5483bd35478a19ef2
akitorahayashi <hayashi20030703@gmail.com>
2025-08-19 16:32:15 +0900

    adminページのカスタマイズ

--- apps/polls/models.py
+++ apps/polls/models.py
@@ -1,8 +1,8 @@
 import datetime
 from django.db import models
 from django.db.models.manager import Manager
+from django.contrib import admin
 from django.utils import timezone
-
 
 class Question(models.Model):
     id: int
@@ -14,6 +14,11 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
