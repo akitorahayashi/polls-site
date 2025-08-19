b443ad9a663aa4a987c9a5dde6c24695b8b9b4d0
akitorahayashi <hayashi20030703@gmail.com>
2025-08-19 15:32:40 +0900

    testsを追加

--- polls-site/settings.py
+++ polls-site/settings.py
@@ -10,11 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import sys
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+sys.path.append(str(BASE_DIR / 'apps'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -37,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.polls.apps.PollsConfig',
+    'polls.apps.PollsConfig',
 ]
 
 MIDDLEWARE = [
